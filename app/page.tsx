8fe53e8a1b04ab9073c89062d78c6437b6cebdd9
Rashmi <rashmi@Rashmis-MacBook-Pro.local>
2023-06-10 09:09:45 +0530

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ import {
 export default async function HomePage() {
   return (
     <section>
-      <h1 className="font-bold text-4xl font-serif">Gaurav Chandak</h1>
+      <h1 className="font-bold text-4xl font-serif">Rashmi Shukla</h1>
       <p className="my-5 max-w-[460px] text-neutral-800 dark:text-neutral-200">
-        Hi, I am a software engineer and entrepreneur. I am currently working at Quizizz as a software engineer.
+        Hi, I am Rashmi. I am currently working at Scaler as a product manager.
       </p>
       <p className="my-5 max-w-[600px] text-neutral-800 dark:text-neutral-200">
-        I previously co-founded an EdTech + HRTech startup called workat.tech. Prior to that I worked at Microsoft, Flipkart and Tracxn.
+        I previously built products at Razorpay and Tracxn.
       </p>
       <p className="my-5 max-w-[600px] text-neutral-800 dark:text-neutral-200">
-        I love to build products that solve user problems. I'm currently learning about building AI-enabled products.
+        I love to build products that solve user problems.
       </p>
       <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-neutral-400">
         <li>
@@ -21,7 +21,7 @@ export default async function HomePage() {
             className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://twitter.com/gc_nit"
+            href="https://twitter.com/oyerashmi"
           >
             <ArrowIcon />
             <p className="h-7">follow me on twitter</p>
@@ -30,7 +30,7 @@ export default async function HomePage() {
             className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://linkedin.com/in/gcnit"
+            href="https://linkedin.com/in/rashmi1808"
           >
             <ArrowIcon />
             <p className="h-7">connect with me on linkedin</p>
